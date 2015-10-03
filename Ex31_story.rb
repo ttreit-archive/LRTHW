8330dad00f7ac4436dee1393a998e23bf51424f1
Tammy Treit <ttreit@gmail.com>
2015-10-03 14:21:01 -0700

    intro function is broken. Need to simplify and test.

--- Ex31_story.rb
+++ Ex31_story.rb
@@ -2,26 +2,63 @@
 
 ### Functions for each section
 
-def prompt
-	print "* "
-end
 
 def hline_top
-	puts "_________________________________________________________"
+	puts "__________________________________________________________"
 end
 
 def hline_bottom
-	puts "---------------------------------------------------------"
+	puts "----------------------------------------------------------"
 end
+
+def ph ### placeholder
+	puts "PLACEHOLDER"
+end
+
+def d_prompt
+	print "* "
+end
+
+=begin
+def d_input(arg1)
+	option = arg1
+	if option == "1"
+		### jump into water story
+		ph
+	elsif option == "2"
+		ph
+	else
+		puts "Please choose one of the options above."
+		puts "I would loop here but I haven't gotten to loops yet."
+end
+=end
 
 def intro
 	puts "As you gain consciousness you blink and look around."
 	puts "You are lying on the rough surface of a dock."
-	puts "You can smell the salt water and hear sea birds squalking"
-	puts "and your eyes adjust to the bright light."
+	puts "You can smell the salt water and hear sea birds squalking."
+	puts "Your eyes adjust to the bright light."
 	puts
 	puts "You realize that you are naked."
-end
+	hline_bottom
+	
+	puts "1. Jump into the water."
+	puts "2. Stand up and stretch."
+	d_prompt
+	d_intro = gets.chomp
+	d_input(d_intro)
+
+	if d_intro == "1"
+		### jump into water story
+		ph
+	elsif d_intro == "2"
+		ph
+	else
+		puts "Please choose one of the options above."
+		puts "I would loop here but I haven't gotten to loops yet."
+end		
+	
+
 
 ### Main Body
 
@@ -33,3 +70,4 @@ hline_bottom
 
 puts
 
+end
