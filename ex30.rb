f366d34a2a5e4b0448cf5bd847c3ff49bac56efd
Tammy Treit <ttreit@gmail.com>
2015-10-03 03:16:24 -0700

    Revised to include more complex boolean expressions per study drills

--- ex30.rb
+++ ex30.rb
@@ -11,6 +11,8 @@ print "Enter the number of trucks: "
 trucks = gets.chomp.to_i
 puts
 
+### old code
+=begin
 if cars > people
 	puts "We should take the cars."
 elsif cars < people
@@ -32,4 +34,24 @@ if people > trucks
 else
 	puts "Fine, lets stay home then."
 end
+=end
+
+### Study Drills use more complex boolean expressions
+
+if cars == people || trucks == cars 
+	puts "We can't decide"
+elsif (trucks < cars || people > trucks) && trucks != 0
+	puts "We should take the trucks."
+elsif
+	if cars > people
+		puts "We should take the cars."
+	elsif cars < people 
+		puts "We should not take the cars."
+	elsif trucks > cars
+		puts "That's too many trucks."
+	end
+else
+	puts "Fine, lets stay home then."
+
+end	
 puts
