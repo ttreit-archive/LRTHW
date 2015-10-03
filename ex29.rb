d549c830f214db362e79e7ee267a67dd382713b5
Tammy Treit <ttreit@gmail.com>
2015-10-03 01:08:22 -0700

    added feature for entering the number of dogs, cats, and people

--- ex29.rb
+++ ex29.rb
@@ -1,35 +1,46 @@
 ### Exercise 29
 
-people = 20
-cats = 30
-dogs = 15
+print "Enter the number of people: "
+people = gets.chomp.to_i
+print "Enter the number of cats: "
+cats = gets.chomp.to_i
+print "Enter the number of dogs: "
+dogs = gets.chomp.to_i
 
 if people < cats 
+	puts
 	puts "Too many cats! The world is doomed!"
 end
 
 if people > cats
+	puts
 	puts "Not many cats! The world is saved!"
 end
 
 if people < dogs
+	puts 
 	puts "The world is drooled on!"
 end
 
 if people > dogs
+	puts 
 	puts "The world is dry!"
 end
 
 dogs += 5
 
-if !(people > dogs || people != dogs)
+if (people > dogs || people == dogs)
+	puts 
 	puts "People are greater than or equal to dogs."
 end
 
 if people <= dogs 
+	puts 
 	puts "People are less than or equal to dogs."
 end
 
 if people == dogs
+	puts 
 	puts "People are dogs."
 end
+puts
